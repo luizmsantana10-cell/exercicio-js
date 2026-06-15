232ca3467e4e01b8d7f4057bb425bf231914e700
Luiz Miguel <luiz.m.santana10@aluno.senai.br>
2026-06-15 15:32:10 -0300

    quest 3 finalizada

--- quest3.js
+++ quest3.js
@@ -3,3 +3,22 @@ Elabore um o algoritmo que possa entrar com nome do produto
 e valor da compra e imprima o nome do produto e o valor
 da venda.
 */
+const prompt = require('prompt-sync')();
+let produto = prompt("Digite o nome: ");
+let valor = Number(prompt("Digite o valor: "));
+let n70 = 0.70
+let n50 = 0.50
+let n40 = 0.40
+let n30 = 0.30
+
+if (valor <= 10){
+    console.log(produto+" lucro de R$"+ valor * n70)
+
+} else if(valor <= 30){
+    console.log(produto+" lucro de R$"+ valor * n50)
+
+} else if( valor <= 50){
+    console.log(produto+" lucro de R$"+ valor * n40)
+}else {
+    console.log(produto+" lucro de R$"+ valor * n30)
+}
