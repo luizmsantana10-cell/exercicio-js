f6bffab50e96f419aa8fab836603d79ecec6bc6e
Luiz Miguel <luiz.m.santana10@aluno.senai.br>
2026-06-15 14:34:12 -0300

    questao 1 finalizada

--- quest1.js
+++ quest1.js
@@ -5,3 +5,12 @@ Escreva um algoritmo que leia um número e informe se ele
 const prompt = require('prompt-sync')();
 
 let num = Number(prompt('Digite um numero: '));
+
+
+
+
+if (num %3 === 0 && num %7 === 0){
+    console.log("É divisível por 3 e 7")
+} else{
+    console.log("Não é divisível por 3 e 7")
+}
