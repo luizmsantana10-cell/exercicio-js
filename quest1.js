093796538597e344788c593230c86b451dfe1b75
Luiz Miguel <luiz.m.santana10@aluno.senai.br>
2026-06-15 14:18:18 -0300

    questao1 iniciada

---
+++ quest1.js
@@ -0,0 +1,7 @@
+/*
+Escreva um algoritmo que leia um número e informe se ele
+é divisível por 3 e por 7.
+*/
+const prompt = require('prompt-sync')();
+
+let num = Number(prompt('Digite um numero: '));
